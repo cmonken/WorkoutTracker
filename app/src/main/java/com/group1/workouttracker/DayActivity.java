0102f81e3c91f38aa8b15a75e891562e2498418e
FenixLilly <john.lillyblad@gmail.com>
2014-07-29 23:18:32 -0700

    Fixed layout on fragment_day.

--- app/src/main/java/com/group1/workouttracker/DayActivity.java
+++ app/src/main/java/com/group1/workouttracker/DayActivity.java
@@ -1,45 +1,65 @@
+/* Some code used adapted from SQLite Tutorial located at
+http://www.vogella.com/tutorials/AndroidSQLite/article.html
+ */
+
 package com.group1.workouttracker;
 
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.view.ContextMenu;
-import android.view.ContextMenu.ContextMenuInfo;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ListView;
 import android.widget.Toast;
-
+import android.widget.ArrayAdapter;
+import java.util.List;
 
 public class DayActivity extends Activity {
+    //does not extend ListActivity, so list functions must be called by myList object
 
-    String buttonClicked;
-    View v;
-    Button reportsBtn;
-    Button helpBtn;
-    Intent intent;
-    //mySQLiteHelper myHelper;
-    TextView summary;
+    private String buttonClicked;
+    private View v;
+    private Button reportsBtn;
+    private Button helpBtn;
+    private Intent intent;
+    private MySQLiteHelper helper;
+    private WorkoutDataSource datasource;
+    private ArrayAdapter<Exercise> adapter;
+    private ListView myList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_day);
-
-        summary = (TextView) findViewById(R.id.textView1);
-        registerForContextMenu(summary);
-
         Intent intent = getIntent();
+        helper = new MySQLiteHelper(this);
         reportsBtn = (Button) findViewById(R.id.button8);
         helpBtn = (Button) findViewById(R.id.button9);
-        /*buttonClicked = intent.getStringExtra("Day");
-        callToast();*/ //used for testing
+        buttonClicked = intent.getStringExtra("Day");
+        //callToast(); //used for testing
+
+        datasource = new WorkoutDataSource(this); //create new datasource
+        datasource.open(); //open the datasource
+
+        ListView myList = (ListView) findViewById(android.R.id.list);
+        //buttonClicked has to be a day to even be in DayActivity
+        List<Exercise> values = datasource.getExercisesFor(buttonClicked);
+
+        // use the SimpleCursorAdapter to show the
+        // elements in a ListView
+        /*ArrayAdapter<Exercise> adapter = new ArrayAdapter<Exercise>(this,
+                android.R.layout.simple_list_item_1, values);
+        myList.setAdapter(adapter); //bind adapter to myList*/
 
         reportsBtn.setOnClickListener(new OnClickListener() {
             @Override
@@ -55,20 +75,15 @@ public class DayActivity extends Activity {
             }
         });
 
-    }
-
-    @Override
-    public void onCreateContextMenu(ContextMenu menu, View v,ContextMenuInfo menuInfo) {
-        super.onCreateContextMenu(menu, v, menuInfo);
-        menu.setHeaderTitle("Summary");
-        menu.add(0, v.getId(), 0, "Edit");
-    }
-
-    @Override
-    public boolean onContextItemSelected(MenuItem item) {
-        if(item.getTitle()=="Edit"){editSummary(item.getItemId());}
-        else {return false;}
-        return true;
+        /*myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        "Click ListItem Number " + position, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });*/
     }
 
     public void myClickHandler(View target) {
@@ -76,19 +91,18 @@ public class DayActivity extends Activity {
         Toast.makeText(getApplicationContext(), buttonClicked, Toast.LENGTH_SHORT).show();
         if (buttonClicked == "Reports") {
             callReportsIntent();
-        }
-        else if (buttonClicked == "Help") {
+        } else if (buttonClicked == "Help") {
             callHelpIntent();
         }
     }
 
-    public void callReportsIntent(){
+    public void callReportsIntent() {
         intent = new Intent(this, ReportsActivity.class);
         intent.putExtra("Reports", buttonClicked); //may remove this line once testing complete
         startActivity(intent);
     }
 
-    public void callHelpIntent(){
+    public void callHelpIntent() {
         intent = new Intent(this, HelpActivity.class);
         intent.putExtra("Help", buttonClicked); //may remove this line once testing complete
         startActivity(intent);
@@ -104,6 +118,25 @@ public class DayActivity extends Activity {
                 break;
         }
     }
+
+    /*public List<Exercise> getAllExercises() {
+        ArrayAdapter<Exercise> adapter = (ArrayAdapter<Exercise>) get();
+        // Select All Query
+        String selectQuery = "SELECT  * FROM contacts";
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                Exercise exercise = new Exercise();
+                exercise.setExercise(cursor.getString(1));
+                // Adding exercise to list
+                exerciseList.add(exercise);
+            } while (cursor.moveToNext());
+        }// return contact list
+        return exerciseList;
+    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -133,11 +166,21 @@ public class DayActivity extends Activity {
         }
     }
 
-    public void editSummary(int id){
-        Toast.makeText(this, "function 1 called", Toast.LENGTH_SHORT).show();
-    }
-
     public void callToast(){
         Toast.makeText(getApplicationContext(),"Entered Day Activity. Button clicked: " + buttonClicked, Toast.LENGTH_LONG).show();
     }
+
+    @Override
+    protected void onResume() {
+        datasource.open();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        datasource.close();
+        super.onPause();
+    }
+
+
 }
