ee5dc0d1fda458ec78d6a71f8000fa89cca648e2
FenixLilly <john.lillyblad@gmail.com>
2014-07-29 22:48:53 -0700

    Trying to get DayActivity adapter to work.

--- app/src/main/java/com/group1/workouttracker/DayActivity.java
+++ app/src/main/java/com/group1/workouttracker/DayActivity.java
@@ -1,8 +1,14 @@
+/* Some code used adapted from SQLite Tutorial located at
+http://www.vogella.com/tutorials/AndroidSQLite/article.html
+ */
+
 package com.group1.workouttracker;
 
 import android.app.Activity;
 import android.app.Fragment;
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
@@ -10,17 +16,26 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import java.util.List;
 
 public class DayActivity extends Activity {
+    //does not extend ListActivity, so list functions must be called by myList object
 
-    String buttonClicked;
-    View v;
-    Button reportsBtn;
-    Button helpBtn;
-    Intent intent;
-    mySQLiteHelper myHelper;
+    private String buttonClicked;
+    private View v;
+    private Button reportsBtn;
+    private Button helpBtn;
+    private Intent intent;
+    private MySQLiteHelper helper = new MySQLiteHelper(this);
+    private WorkoutDataSource datasource;
+    private boolean test;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +44,21 @@ public class DayActivity extends Activity {
         Intent intent = getIntent();
         reportsBtn = (Button) findViewById(R.id.button8);
         helpBtn = (Button) findViewById(R.id.button9);
-        /*buttonClicked = intent.getStringExtra("Day");
-        callToast();*/ //used for testing
+        buttonClicked = intent.getStringExtra("Day");
+        //callToast(); //used for testing
+
+        datasource = new WorkoutDataSource(this); //create new datasource
+        datasource.open(); //open the datasource
+
+        ListView myList = (ListView) findViewById(android.R.id.list);
+        //buttonClicked has to be a day to even be in DayActivity
+        List<Exercise> values = datasource.getExercisesFor(buttonClicked);
+
+        // use the SimpleCursorAdapter to show the
+        // elements in a ListView
+        ArrayAdapter<Exercise> adapter = new ArrayAdapter<Exercise>(this,
+                android.R.layout.simple_list_item_1, values);
+        myList.setAdapter(adapter); //bind adapter to myList
 
         reportsBtn.setOnClickListener(new OnClickListener() {
             @Override
@@ -46,6 +74,15 @@ public class DayActivity extends Activity {
             }
         });
 
+        /*myList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                Toast.makeText(getApplicationContext(),
+                        "Click ListItem Number " + position, Toast.LENGTH_LONG)
+                        .show();
+            }
+        });*/
     }
 
     public void myClickHandler(View target) {
@@ -53,19 +90,18 @@ public class DayActivity extends Activity {
         Toast.makeText(getApplicationContext(), buttonClicked, Toast.LENGTH_SHORT).show();
         if (buttonClicked == "Reports") {
             callReportsIntent();
-        }
-        else if (buttonClicked == "Help") {
+        } else if (buttonClicked == "Help") {
             callHelpIntent();
         }
     }
 
-    public void callReportsIntent(){
+    public void callReportsIntent() {
         intent = new Intent(this, ReportsActivity.class);
         intent.putExtra("Reports", buttonClicked); //may remove this line once testing complete
         startActivity(intent);
     }
 
-    public void callHelpIntent(){
+    public void callHelpIntent() {
         intent = new Intent(this, HelpActivity.class);
         intent.putExtra("Help", buttonClicked); //may remove this line once testing complete
         startActivity(intent);
@@ -81,6 +117,25 @@ public class DayActivity extends Activity {
                 break;
         }
     }
+
+    /*public List<Exercise> getAllExercises() {
+        ArrayAdapter<Exercise> adapter = (ArrayAdapter<Exercise>) get();
+        // Select All Query
+        String selectQuery = "SELECT  * FROM contacts";
+        SQLiteDatabase db = helper.getWritableDatabase();
+        db.
+        Cursor cursor = db.rawQuery(selectQuery, null);
+        // looping through all rows and adding to list
+        if (cursor.moveToFirst()) {
+            do {
+                Exercise exercise = new Exercise();
+                exercise.setExercise(cursor.getString(1));
+                // Adding exercise to list
+                exerciseList.add(exercise);
+            } while (cursor.moveToNext());
+        }// return contact list
+        return exerciseList;
+    }*/
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -113,4 +168,18 @@ public class DayActivity extends Activity {
     public void callToast(){
         Toast.makeText(getApplicationContext(),"Entered Day Activity. Button clicked: " + buttonClicked, Toast.LENGTH_LONG).show();
     }
+
+    @Override
+    protected void onResume() {
+        datasource.open();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        datasource.close();
+        super.onPause();
+    }
+
+
 }
